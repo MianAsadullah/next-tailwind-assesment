63821b6be9ba56a28798603956ee1da69a555503
Asadullah <this.asadullah@gmail.com>
2024-10-25 02:50:22 +0500

    70% completed

---
+++ components/Main.js
@@ -0,0 +1,250 @@
+export default function Main() {
+  return (<>
+    <div className="w-full h-screen bg-image ">
+
+    </div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center   ">
+        <div className="text-2xl mt-6 leading-8 title">
+          영상 제작소
+        </div>
+        <div className="font-extralight text-white mt-4 text-5xl leading-[66px]" >
+          당장 내일 {""}
+          <span className="font-bold  text-[44px]">
+            ‘고품질 맞춤 영상’
+          </span>
+          을 받아보세요.
+        </div>
+
+        <div className="flex">
+          <div className="rounded-full video-section-circle ">
+            <div>
+              <div>
+                FAST
+              </div>
+              <div>
+                X2
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="text-white text-3xl font-bold">
+          AI VIDEO ON YOUR DEMAND
+        </div>
+        <p className="font-bold text-xl leading-8 mt-6">
+          가상인간 뿐 아니라 배경 이미지와 음악 생성 등 국내 최고의 생성 AI 기술에 <br />
+          영상 전문가의 터치를 더해 비용 및 시간은 절감하고 퀄리티는 보장된 영상을 만듭니다. <br /><br />
+
+          가격은 절반으로, 속도는 두배로, 품질은 A++! <br />
+          기업 홍보, 제품 소개, 교육 및 행사, 정보 및 안내 영상 등 <br />
+          당신이 필요한 '그 영상'을 플루닛 영상제작소에 맡겨주세요. <br />
+        </p>
+      </div>
+    </div>
+
+
+    <section className="h-screen text-center">
+      <div class="flex items-center justify-center space-x-2">
+        <span class="inline-block w-4 h-4 bg-gradient-to-r -mt-8 from-pink-500 to-yellow-500 rounded-full"></span>
+
+        <h2 className="relative font-bold text-5xl text-white">PREVIOUS WORKS
+
+
+        </h2>
+      </div>
+
+
+
+
+      <div className="grid  grid-cols-2 lg:grid-cols-4  mt-12">
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork1.png"
+            />
+            <div className="absolute top-0 right-0 bg-indigo-500 text-white font-bold px-2 py-1 m-2 rounded-md">
+              New
+            </div>
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork2.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork3.png"
+            />
+            <div className="absolute top-0 right-0 bg-indigo-500 text-white font-bold px-2 py-1 m-2 rounded-md">
+              New
+            </div>
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className="  overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork4.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork5.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork6.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork07.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork08.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork9.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork10.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork11.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+
+        <div className=" overflow-hidden">
+          <div className="relative">
+            <img
+              className="w-full h-[270px] object-cover"
+              src="/images/prevWork12.png"
+            />
+            <div className="absolute bottom-0 right-0 bg-gray-800 text-white px-2 py-1 m-2 rounded-md text-xs">
+              3 min read
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+    </section>
+
+    <section className="h-screen text-center ">
+      <div class="flex items-center justify-center space-x-2 mt-6">
+        <span class="inline-block w-4 h-4 bg-gradient-to-r -mt-8 from-pink-500 to-yellow-500 rounded-full"></span>
+
+        <h2 className="relative font-bold text-5xl text-white">MAKING PROCESS
+
+
+        </h2>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div className="border border-white h-20 w-20" ></div>
+
+      </div>
+    </section>
+    <div style={{ wordBreak: "keep-all" }} className="fixed hidden lg:flex    justify-center items-center text-center right-8 top-80 rounded-full p-2 font-bold text-lg text-white  h-28 w-28 custom-gradient  ">
+
+      <div style={{ textAlign: "-webkit-center" }} className="mt-1">
+        <div>
+          제작
+          문의하기
+        </div>
+        <img className="text-center self-center" src="/images/Isolation-icon.png" alt="icon" width={20} height={20} />
+
+      </div>
+
+    </div>
+  </ >
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{html,js,jsx,ts,tsx}', // Add paths to your content files here
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
